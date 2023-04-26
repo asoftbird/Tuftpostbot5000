435208dc61001f066617b827b2f7e5f0d2e92ecf
asoftbird <asoftbird@gmail.com>
2023-04-26 14:18:22 +0200

    add todo

--- util/helpers.py
+++ util/helpers.py
@@ -14,6 +14,7 @@ elif sys.platform.startswith("win32"):
     pathlib.PosixPath = pathlib.WindowsPath
 
 # decorators
+# todo: use kwargs to ensure compatibility with other functions
 def DCcheckOutfileExists(func):
     def wrapper(filename, data):
         if not os.path.isfile(filename):
